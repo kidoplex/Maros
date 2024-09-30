592f4c53f3d1dbe6037198c13b167e4419030527
Maros <maros7929@gmail.com>
2024-09-30 12:32:03 +0200

    NAVBAR

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-mport React from 'react';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
